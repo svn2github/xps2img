77cf8c8f3ef9963559737a3198ad3b777585e3ad
i2van <i2van@f56d0dea-3dc4-4344-980a-dda9636af30f>
2011-08-20 17:57:35 +0000

    git-svn-id: svn://svn.code.sf.net/p/xps2img/code/trunk@73 f56d0dea-3dc4-4344-980a-dda9636af30f

--- build.bat
+++ build.bat
@@ -1,7 +1,15 @@
 @echo off
 
 set msbuild="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
-set buildOptions=/p:Configuration=Release /t:Rebuild
+
+set buildConfig=Release
+::set buildConfig=Debug
+
+set buildOptions=/p:Configuration=%buildConfig% /t:Rebuild
+
+set PF=%PROGRAMFILES%
+
+if defined PROCESSOR_ARCHITEW6432 set PF=%PROGRAMFILES(x86)%
 
 %msbuild% Xps2Img.sln %buildOptions% || exit /b 1
 %msbuild% Xps2ImgUI.sln %buildOptions% || exit /b 1
