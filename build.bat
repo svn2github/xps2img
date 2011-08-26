0fc287c021d3c3ea51dfd7e70ce4cc867c868a02
i2van <i2van@f56d0dea-3dc4-4344-980a-dda9636af30f>
2011-08-26 19:46:51 +0000

    [+] HTML help.
    git-svn-id: svn://svn.code.sf.net/p/xps2img/code/trunk@76 f56d0dea-3dc4-4344-980a-dda9636af30f

--- build.bat
+++ build.bat
@@ -1,15 +1,38 @@
 @echo off
 
-set msbuild="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
-
 set buildConfig=Release
 ::set buildConfig=Debug
 
+set slnFolder=%~dp0
+set helpFolder=%slnFolder%Help
+set outFolder=%slnFolder%_bin\%buildConfig%
+
+set PFx86=%PROGRAMFILES(x86)%
+if "%PFx86%"=="" set PFx86=%PROGRAMFILES%
+
+set msbuild="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+set hhc="%PFx86%\HTML Help Workshop\hhc.exe"
+
 set buildOptions=/p:Configuration=%buildConfig% /t:Rebuild
 
-set PF=%PROGRAMFILES%
+@echo on
 
-if defined PROCESSOR_ARCHITEW6432 set PF=%PROGRAMFILES(x86)%
+%msbuild% "%slnFolder%Xps2Img.sln" %buildOptions% || goto ERROR
+%msbuild% "%slnFolder%Xps2ImgUI.sln" %buildOptions% || goto ERROR
 
-%msbuild% Xps2Img.sln %buildOptions% || exit /b 1
-%msbuild% Xps2ImgUI.sln %buildOptions% || exit /b 1
+%hhc% "%helpFolder%\xps2img.hhp" && goto ERROR
+copy "%helpFolder%\xps2img.chm" "%outFolder%" /Y
+
+@echo off
+
+echo.
+echo All OK. Output folder is '%outFolder%'
+exit /b 0
+
+:ERROR
+@echo off
+echo.
+echo.
+echo There were errors!
+exit /b 1
+
