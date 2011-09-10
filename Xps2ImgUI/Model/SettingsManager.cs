24cb57429956fb14b7b0b5ffd031c4426effa526
i2van <i2van@f56d0dea-3dc4-4344-980a-dda9636af30f>
2011-09-10 17:19:36 +0000

    git-svn-id: svn://svn.code.sf.net/p/xps2img/code/trunk@121 f56d0dea-3dc4-4344-980a-dda9636af30f

--- Xps2ImgUI/Model/SettingsManager.cs
+++ Xps2ImgUI/Model/SettingsManager.cs
@@ -10,34 +10,49 @@ namespace Xps2ImgUI.Model
 {
     public static class SettingsManager
     {
-        private const string optionsSeparator = "\x20";
+        private const char optionsSeparator = '\x20';
         private const string autoFileName = "yyyyMMdd-HHmmss";
+
+        private static OpenFileDialog _loadDialog;
+        private static SaveFileDialog _saveDialog;
 
         public static Xps2ImgModel LoadSettings()
         {
-            var initialDirectory = EnsureApplicationDataFolder();
-            using (var dialog = new OpenFileDialog { Filter = Resources.Strings.XPS2ImgFilesFilter + Utils.Filter.AllFiles, InitialDirectory = initialDirectory })
+            if (_loadDialog == null)
             {
-                if (dialog.ShowDialog() == DialogResult.OK)
-                {
-                    var commandLine = String.Join(optionsSeparator, File.ReadAllLines(dialog.FileName));
-                    return new Xps2ImgModel(Parser.Parse<Options>(commandLine.Split(optionsSeparator[0]), Parser.ApplicationName, true));
-                }
+                InitFileDialog(_loadDialog = new OpenFileDialog());
             }
+
+            if (_loadDialog.ShowDialog() == DialogResult.OK)
+            {
+                var commandLine = String.Join(optionsSeparator.ToString(), File.ReadAllLines(_loadDialog.FileName));
+                // TODO: Parse command line
+                return new Xps2ImgModel(Parser.Parse<Options>(commandLine.Split(optionsSeparator), Parser.ApplicationName, true));
+            }
+
             return null;
         }
 
         public static void SaveSettings(Xps2ImgModel xps2ImgModel)
         {
-            var initialDirectory = EnsureApplicationDataFolder();
-            var fileName = DateTime.Now.ToString(autoFileName);
-            using (var dialog = new SaveFileDialog { Filter = Resources.Strings.XPS2ImgFilesFilter + Utils.Filter.AllFiles, InitialDirectory = initialDirectory, FileName = fileName })
+            if (_saveDialog == null)
             {
-                if (dialog.ShowDialog() == DialogResult.OK)
-                {
-                    File.WriteAllText(dialog.FileName, xps2ImgModel.FormatCommandLine());
-                }
+                InitFileDialog(_saveDialog = new SaveFileDialog());
             }
+
+            _saveDialog.FileName = DateTime.Now.ToString(autoFileName);
+
+            if (_saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(_saveDialog.FileName, xps2ImgModel.FormatCommandLine());
+            }
+        }
+
+        private static void InitFileDialog(FileDialog fileDialog)
+        {
+            fileDialog.Filter = Resources.Strings.XPS2ImgFilesFilter + Utils.Filter.AllFiles;
+            fileDialog.InitialDirectory = EnsureApplicationDataFolder();
+            fileDialog.RestoreDirectory = true;
         }
 
         private static string EnsureApplicationDataFolder()
