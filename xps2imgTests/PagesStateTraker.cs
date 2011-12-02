c0cbb4a336e4156aded87a4122b4df35ca81bcab
i2van <i2van@f56d0dea-3dc4-4344-980a-dda9636af30f>
2011-12-02 20:57:12 +0000

    git-svn-id: svn://svn.code.sf.net/p/xps2img/code/trunk@219 f56d0dea-3dc4-4344-980a-dda9636af30f

---
+++ xps2imgTests/PagesStateTraker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Linq;
+
+namespace Xps2ImgTests
+{
+    public class PagesStateTraker
+    {
+        private BitArray _pages;
+        private BitArray _processedPages;
+
+        public void Start(BitArray pages)
+        {
+            _pages = pages;
+            _processedPages = new BitArray(pages);
+        }
+
+        public void Set(int index)
+        {
+            _processedPages.Set(index, true);
+        }
+
+        public bool HasUnprocessed
+        {
+            get { return _processedPages.Cast<bool>().Any(b => b); }
+        }
+    }
+}
---
+++ xps2imgTests/PagesStateTrakerUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Xps2ImgTests
+{
+    [TestClass]
+    public class PagesStateTrakerUnitTest
+    {
+        [TestMethod]
+        public void TrackPagesState()
+        {
+            var pagesStateTracker = new PagesStateTraker();
+
+            pagesStateTracker.Start(new BitArray(new byte[]{ 0x01, 0x43, 0x00 }));
+            Assert.IsTrue(pagesStateTracker.HasUnprocessed);
+
+            pagesStateTracker.Start(new BitArray(new[] { true }));
+            Assert.IsTrue(pagesStateTracker.HasUnprocessed);
+
+            pagesStateTracker.Start(new BitArray(new byte[] { 0x00, 0x00, 0x00 }));
+            Assert.IsFalse(pagesStateTracker.HasUnprocessed);
+        }
+    }
+}
