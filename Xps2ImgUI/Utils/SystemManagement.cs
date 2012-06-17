97668c18564fb6b5f452408d42a2fff9559dbd4d
i2van <i2van@f56d0dea-3dc4-4344-980a-dda9636af30f>
2012-06-17 16:48:50 +0000

    [+] Improved system power management.
    git-svn-id: svn://svn.code.sf.net/p/xps2img/code/trunk@359 f56d0dea-3dc4-4344-980a-dda9636af30f

--- Xps2ImgUI/Utils/SystemManagement.cs
+++ Xps2ImgUI/Utils/SystemManagement.cs
@@ -1,23 +1,58 @@
-﻿using System.Management;
+﻿using System;
+using System.Management;
+using System.Windows.Forms;
 
 namespace Xps2ImgUI.Utils
 {
+    [Flags]
     public enum ShutdownType
     {
+        Forced          = 4,
         LogOff          = 0,
         Shutdown        = 1,
         Reboot          = 2,
-        PowerOff        = 8,
-        ForcedLogOff    = 4,
-        ForcedShutdown  = 5,
-        ForcedReboot    = 6,
-        ForcedPowerOff  = 12
+        Hibernate       = 8,
+        Suspend         = 16,
+        ForcedLogOff    = LogOff    | Forced,
+        ForcedShutdown  = Shutdown  | Forced,
+        ForcedReboot    = Reboot    | Forced,
+        ForcedHibernate = Hibernate | Forced,
+        ForcedSuspend   = Suspend   | Forced
     };
 
-    public static class SystemManagement
+    public static partial class SystemManagement
     {
-        public static void Shutdown(ShutdownType shutdownType)
+        public static bool CanHibernate
         {
+            get { return GetPwrCapabilities().HiberFilePresent; }
+        }
+
+        public static bool CanSleep
+        {
+            get { return GetPwrCapabilities().SleepButtonPresent; }
+        }
+
+        public static bool CanShutdown
+        {
+            get { return GetPwrCapabilities().PowerButtonPresent; }
+        }
+
+        public static void Shutdown(ShutdownType shutdownType, bool disableWakeEvent = false)
+        {
+            var isForced = ((int)shutdownType & (int)ShutdownType.Forced) != 0;
+
+            if ((ShutdownType.Hibernate & shutdownType) != 0)
+            {
+                Application.SetSuspendState(PowerState.Hibernate, isForced, disableWakeEvent);
+                return;		
+            }
+            
+            if ((ShutdownType.Suspend & shutdownType) != 0)
+            {
+                Application.SetSuspendState(PowerState.Suspend, isForced, disableWakeEvent);
+                return;		
+            }
+
             using (var operatingSystem = new ManagementClass("Win32_OperatingSystem"))
             {
                 operatingSystem.Get();
@@ -27,7 +62,7 @@ namespace Xps2ImgUI.Utils
                 var args = operatingSystem.GetMethodParameters("Win32Shutdown");
 
                 args["Flags"] = shutdownType;
-                args["Reserved"] = "0";
+                args["Reserved"] = 0;
 
                 foreach (ManagementObject managementObject in operatingSystem.GetInstances())
                 {
