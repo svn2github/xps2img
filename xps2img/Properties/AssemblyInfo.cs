adc8741c754e9252987cb2b97b0372ebc2723ef3
i2van <i2van@f56d0dea-3dc4-4344-980a-dda9636af30f>
2012-11-03 19:35:48 +0000

    [*] Version number.
    git-svn-id: svn://svn.code.sf.net/p/xps2img/code/trunk@520 f56d0dea-3dc4-4344-980a-dda9636af30f

--- xps2img/Properties/AssemblyInfo.cs
+++ xps2img/Properties/AssemblyInfo.cs
@@ -43,5 +43,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly : AssemblyVersion    ("3.20.0.0")]
-[assembly : AssemblyFileVersion("3.20.0.0")]
+[assembly : AssemblyVersion    ("3.21.0.0")]
+[assembly : AssemblyFileVersion("3.21.0.0")]
