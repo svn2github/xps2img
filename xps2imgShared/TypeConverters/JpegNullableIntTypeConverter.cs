d9db3c5fbd4297da4993d699a72e4513428305ce
i2van <i2van@f56d0dea-3dc4-4344-980a-dda9636af30f>
2012-12-09 14:21:45 +0000

    [+] Jpeg quality values list.
    git-svn-id: svn://svn.code.sf.net/p/xps2img/code/trunk@542 f56d0dea-3dc4-4344-980a-dda9636af30f

--- xps2imgShared/TypeConverters/JpegNullableIntTypeConverter.cs
+++ xps2imgShared/TypeConverters/JpegNullableIntTypeConverter.cs
@@ -1,9 +1,26 @@
-﻿using Xps2Img.Shared.CommandLine;
+﻿using System.ComponentModel;
+
+using Xps2Img.Shared.CommandLine;
 
 namespace Xps2Img.Shared.TypeConverters
 {
     public class JpegNullableIntTypeConverter : CheckedNullableIntTypeConverter
     {
-        public JpegNullableIntTypeConverter() : base(Validation.JpegQualityValidationExpression) { }
+        public JpegNullableIntTypeConverter()
+            : base(Validation.JpegQualityValidationExpression)
+        {
+        }
+
+        private static readonly int[] Dpis = new[] { 10, 15, 25, 35, 45, 55, 65, 75, 85, 95, 100 };
+
+        public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            return new StandardValuesCollection(Dpis);
+        }
     }
 }
