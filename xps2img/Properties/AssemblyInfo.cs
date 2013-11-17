64cf6c3b3c562ca46f17d4bb38887a9decfdb591
i2van <i2van@f56d0dea-3dc4-4344-980a-dda9636af30f>
2013-11-17 18:53:05 +0000

    [*] Adjusted MainForm height.
    git-svn-id: svn://svn.code.sf.net/p/xps2img/code/trunk@636 f56d0dea-3dc4-4344-980a-dda9636af30f

--- xps2img/Properties/AssemblyInfo.cs
+++ xps2img/Properties/AssemblyInfo.cs
@@ -47,5 +47,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly : AssemblyVersion    ("3.27.0.0")]
-[assembly : AssemblyFileVersion("3.27.0.0")]
+[assembly : AssemblyVersion    ("3.28.0.0")]
+[assembly : AssemblyFileVersion("3.28.0.0")]
