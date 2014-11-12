5c38ea25b67b5e2a3ebcdba124289c4c4ccdca6e
i2van <i2van@f56d0dea-3dc4-4344-980a-dda9636af30f>
2014-11-12 17:44:37 +0000

    [+] Lang merge script.
    git-svn-id: svn://svn.code.sf.net/p/xps2img/code/trunk@702 f56d0dea-3dc4-4344-980a-dda9636af30f

---
+++ _Build/Scripts/MergeResources.bat
@@ -0,0 +1,43 @@
+@echo off
+
+set scriptDir=%~dp0
+
+set targetFile=%~1
+set targetDir=%~dp1
+set targetName=%~n1
+set targetExt=%~x1
+
+set ilMerge=%scriptDir%..\Bin\ILMerge.exe
+
+set startFile=%~1
+
+set outType=winexe
+
+if /i "%targetExt%"==".dll" set outType=library
+
+call :mergeLang "uk"
+
+exit 0
+
+:ERROR
+@echo off
+echo ILMerge failed with errors.
+exit 1
+
+:mergeLang
+set lang=%~1
+set tempName=%targetName%_temp_%lang%_%targetExt%
+set tempFile=%targetDir%\%tempName%
+set resFile=%targetDir%\%lang%\%targetName%.resources.dll
+echo Merging "%lang%" language...
+if not exist %resFile% (
+	echo Warning: "%lang%" does not exist.
+	exit /b 1
+)
+@echo on
+ren "%targetFile%" "%tempName%" || goto ERROR
+"%ilMerge%" "/out:%startFile%" /t:%outType% "%tempFile%" "%resFile%" || goto ERROR
+@echo off
+rd /s /q "%targetDir%\%lang%"
+del /q "%targetDir%\%targetName%_temp_*_%targetExt%"
+exit /b 0
