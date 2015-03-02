3a1e31ecd730f1c756fe1a96c975e48cae5c629c
i2van <i2van@f56d0dea-3dc4-4344-980a-dda9636af30f>
2015-03-02 21:29:39 +0000

    [*] Simplified single assembly resource manager. [*] Ukrainian texts.
    git-svn-id: svn://svn.code.sf.net/p/xps2img/code/trunk@824 f56d0dea-3dc4-4344-980a-dda9636af30f

--- _Lib/CommandLine/Localization/SingleAssemblyResourceManager.cs
+++ _Lib/CommandLine/Localization/SingleAssemblyResourceManager.cs
@@ -1,82 +1,47 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Reflection;
 using System.Resources;
 
 namespace CommandLine.Localization
 {
-    // http://stackoverflow.com/questions/1952638/single-assembly-multi-language-windows-forms-deployment-ilmerge-and-satellite-a
     public class SingleAssemblyResourceManager : ResourceManager
     {
-        private readonly Type _contextTypeInfo;
-        private CultureInfo _neutralResourcesCulture;
+        private readonly Type _type;
+        private readonly Dictionary<string, ResourceSet> _resourceSets = new Dictionary<string, ResourceSet>();
 
-        private static readonly Type ResourceManagerType = typeof(ResourceManager);
-
-        private static readonly MethodInfo AddResourceSetMethodInfo;
-        private static readonly bool IsNet4;
-
-        private readonly FieldInfo _resourceSetsFieldInfo;
-
-        static SingleAssemblyResourceManager()
+        public SingleAssemblyResourceManager(Type type)
+            : base(type)
         {
-            Func<Type, Type, MethodInfo> getAddResourceSetMethodInfo = (p1, p2) => ResourceManagerType.GetMethod("AddResourceSet", BindingFlags.Static | BindingFlags.NonPublic, null, new[] { p1, p2, typeof(ResourceSet).MakeByRefType() }, null);
-
-            AddResourceSetMethodInfo = getAddResourceSetMethodInfo(typeof(Hashtable), typeof(CultureInfo));
-
-            IsNet4 = AddResourceSetMethodInfo == null;
-            if (IsNet4)
-            {
-                AddResourceSetMethodInfo = getAddResourceSetMethodInfo(typeof(Dictionary<string, ResourceSet>), typeof(string));
-            }
-        }
-
-        public SingleAssemblyResourceManager(Type type) : base(type)
-        {
-            _contextTypeInfo = type;
-
-            if (IsNet4)
-            {
-                _resourceSetsFieldInfo = ResourceManagerType.GetField("_resourceSets", BindingFlags.Instance | BindingFlags.NonPublic);
-            }
+            _type = type;
         }
 
         protected override ResourceSet InternalGetResourceSet(CultureInfo culture, bool createIfNotExists, bool tryParents)
         {
-            var resourceSet = (ResourceSet)ResourceSets[culture];
-
-            if (resourceSet != null)
+            lock (_resourceSets)
             {
+                var cultureName = culture.Name;
+
+                ResourceSet resourceSet;
+
+                if (_resourceSets.TryGetValue(cultureName, out resourceSet))
+                {
+                    return resourceSet;
+                }
+
+                var resourceFileName = GetResourceFileName(culture);
+
+                var manifestResourceStream = MainAssembly.GetManifestResourceStream(_type, resourceFileName) ??
+                                             MainAssembly.GetManifestResourceStream(resourceFileName);
+
+                resourceSet = manifestResourceStream != null
+                                ? new ResourceSet(manifestResourceStream)
+                                : base.InternalGetResourceSet(culture, createIfNotExists, tryParents);
+
+                _resourceSets.Add(cultureName, resourceSet);
+
                 return resourceSet;
             }
-
-            if (_neutralResourcesCulture == null)
-            {
-                _neutralResourcesCulture = GetNeutralResourcesLanguage(MainAssembly);
-            }
-
-            if (_neutralResourcesCulture.Equals(culture))
-            {
-                culture = CultureInfo.InvariantCulture;
-            }
-
-            var resourceFileName = GetResourceFileName(culture);
-
-            var store = MainAssembly.GetManifestResourceStream(_contextTypeInfo, resourceFileName);
-
-            if (store == null || AddResourceSetMethodInfo == null)
-            {
-                return base.InternalGetResourceSet(culture, createIfNotExists, tryParents);
-            }
-
-            var parameters = new [] { IsNet4 ? _resourceSetsFieldInfo.GetValue(this) : ResourceSets, IsNet4 ? (object)culture.Name : culture, new ResourceSet(store) };
-
-            AddResourceSetMethodInfo.Invoke(this, parameters);
-
-            return (ResourceSet)parameters.Last();
         }
     }
 }
