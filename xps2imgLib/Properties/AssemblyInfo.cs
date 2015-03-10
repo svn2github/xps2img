ff1d430b4db9f694e16c0ef1d25c1340232bb346
i2van <i2van@f56d0dea-3dc4-4344-980a-dda9636af30f>
2015-03-10 22:37:56 +0000

    [+] xps2imgLib
    git-svn-id: svn://svn.code.sf.net/p/xps2img/code/trunk@834 f56d0dea-3dc4-4344-980a-dda9636af30f

---
+++ xps2imgLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("http://xps2img.sf.net")]
+[assembly: AssemblyProduct("Xps2Img")]
+[assembly: AssemblyCopyright("Copyright © 2010-2015, Ivan Ivon")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly : ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly : Guid("a9a56726-7a2e-4e55-ae35-4e87c8673067")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly : AssemblyVersion    ("4.0.0.0")]
+[assembly : AssemblyFileVersion("4.0.0.0")]
